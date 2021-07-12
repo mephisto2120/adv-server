148c216b6cb77f6ed3d0c0d9e0ebd9251602ebe1
mephisto2120 <mephisto2120@gmail.com>
2021-07-12 21:58:32 +0200

    Updating documentation

--- README.md
+++ README.md
@@ -5,7 +5,12 @@
 * In order to query data please use swagger UI for doing it in convenient way:
   http://advserver-env.eba-hjmcippm.us-east-2.elasticbeanstalk.com/swagger-ui.html#
 
-* Tech stack: Java 8, MyBatis, Maria DB, Spring Boot, Swagger, Docker
+* Tech stack: Java 8, MyBatis, Maria DB, Spring Boot, Swagger, Docker, maven
 
 * CI/CD: travis is responsible for unit tests and deploying app to dockerhub and AWS Elastic Beanstalk:
   https://www.travis-ci.com/github/mephisto2120/adv-server
+* In order to execute integration tests db instance is required. It can be created by invoking script:  
+  src/main/resources/import.sql from project https://github.com/mephisto2120/adv-importer. 
+  Moreover, there is an assumption, that data is uploaded from mentioned project from path:
+  src/test/resources/dataToImport.csv. It can be easily done by invoking integration test
+  com.tryton.adv.importer.service.CsvImporterIntegrationTest#importBigCsv.
--- src/main/java/com/tryton/adv/server/controller/AdvertisementController.java
+++ src/main/java/com/tryton/adv/server/controller/AdvertisementController.java
@@ -1,6 +1,8 @@
 package com.tryton.adv.server.controller;
 
 import com.tryton.adv.server.service.AdvertisementService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import static java.util.Objects.requireNonNull;
 
 @RestController
 @RequestMapping(value = "/advertisement")
+@Api(value = "Calculates metrics")
 public class AdvertisementController {
 
     private final AdvertisementService advertisementService;
@@ -25,6 +28,7 @@ public class AdvertisementController {
     }
 
     @GetMapping("/total-clicks")
+    @ApiOperation(value = "Calculates total clicks for a given Datasource for a given Date range")
     public long geTotalClicks(@RequestParam String dataSource,
                               @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
                               @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Optional<LocalDate> endDateOptional) {
@@ -32,12 +36,14 @@ public class AdvertisementController {
     }
 
     @GetMapping("/click-through-rate")
+    @ApiOperation(value = "Calculates Click-Through Rate (CTR) per Datasource and Campaign")
     public long getClickThroughRate(@RequestParam String dataSource,
                                     @RequestParam String campaign) {
         return advertisementService.getClickThroughRate(dataSource, campaign);
     }
 
     @GetMapping("/impressions")
+    @ApiOperation(value = "Calculates impressions over time (daily)")
     public long getImpressions(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return advertisementService.getImpressions(date);
     }
